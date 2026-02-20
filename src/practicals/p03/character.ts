786429a494d0683e2fb8de968e326848b033a5e6
Zer00ne <winnerfahsai@gmail.com>
2026-02-20 18:16:05 +0700

    add

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,18 @@
-export class Character {}
+export class Character {
+    protected name: string;
+    protected health: number;
+
+    constructor(name: string, health: number = 100) {
+        this.name = name;
+        this.health = health;
+    }
+    getName(): string {
+        return this.name;
+    }
+    getHealth(): number {
+        return this.health;
+    }
+    receiveDamage(damage: number): void {
+        this.health = this.health - damage;
+    }
+}
