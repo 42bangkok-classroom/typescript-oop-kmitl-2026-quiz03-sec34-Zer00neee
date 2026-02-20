c436ffa0b83d8fe4e0153ad949cbd652e311d76c
Zer00ne <winnerfahsai@gmail.com>
2026-02-20 18:06:23 +0700

    add

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,13 @@
-export class Car {}
+export class Car {
+    brand: string;
+    model: string;
+
+    constructor(brand: string, model: string) {
+        this.brand = brand;
+        this.model = model;
+    }
+
+    start(): void {
+        console.log(this.brand + " " + this.model + " is starting...");
+    }
+}
--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,32 @@
-export class User {}
+export class User {
+    private firstname: string;
+    private lastname: string;
+    private age: number;
+    public static BIRTH_YEAR: number = 2000;
+
+    constructor() {
+        this.firstname = "";
+        this.lastname = "";
+        this.age = 0;
+    }
+
+    setFirstname(name: string): void {
+        this.firstname = name;
+    }
+
+    setLastname(name: string): void {
+        this.lastname = name;
+    }
+
+    setAge(age: number): void {
+        this.age = age;
+    }
+
+    getAge(): number {
+        return this.age;
+    }
+
+    getFullName(): string {
+        return this.firstname + " " + this.lastname;
+    }
+}
