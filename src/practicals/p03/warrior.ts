d24316eff7437b8946fbedd1e7dfd5a4e08f2744
Zer00ne <winnerfahsai@gmail.com>
2026-02-20 18:18:48 +0700

    add

--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,18 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
-  // TODO: implement class properties, constructor with super(...), and methods
+    private weapon: string;
+
+    constructor(name: string, weapon: string, health: number) {
+        super(name, health);
+        this.weapon = weapon;
+    }
+
+    getWeapon(): string {
+        return this.weapon;
+    }
+    override receiveDamage(damage: number): void {
+        let reducedDamage = damage * 0.9;
+        this.health = this.health - reducedDamage;
+    }
 }
